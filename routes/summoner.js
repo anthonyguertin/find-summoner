c86a50b8b5bcb251b2aae6497bb4e03683a2478e
Anthony Guertin <anthony.guertin@gmail.com>
2017-03-03 21:06:25 -0700

    Update summoner.js

--- routes/summoner.js
+++ routes/summoner.js
@@ -41,7 +41,7 @@ module.exports = function(app) {
         var options = 
         {
             "Content-Type": "application/json",
-            "url": "https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/" + summoner_id + "/summary?season=SEASON2016&api_key=RGAPI-e935b707-0a8a-4c2d-a4a0-840477812e08"
+            "url": "https://na.api.pvp.net/api/lol/na/v1.3/stats/by-summoner/" + summoner_id + "/summary?season=SEASON2016&api_key="
         }
 
         request.get(options, function(err, httpResponse, body) {
